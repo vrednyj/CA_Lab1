8597e8a231c45e4f1519b0ca1a8dfdca4ba7fac9
Vitaliy Baseckas <L00169827@student.lyit.ie>
2021-10-13 22:26:07 +0100

    dd2

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from pkg import CA_Lab1
 
 class TestMethods(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(CA_Lab1.run(), 'This will print something to the screen')
+        self.assertEqual(CA_Lab1.run(), 'This will print something to the scree')
 
 
 if __name__ == '__main__':

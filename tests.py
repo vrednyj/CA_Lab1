99e69cc493151c5536f250738b11595434d737e8
Vitaliy Baseckas <L00169827@student.lyit.ie>
2021-10-14 00:23:43 +0100

    Final

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(CA_Lab1.run(), 'This will print something to the screen',"Test 1 PASS")
         self.assertEqual(Maths.numbers(), "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]","Test 2 PASS")
         self.assertNotEqual(Maths.numbers(), "[1, 2, 3, 4, 5, 6, 7, 8, 0]","Test 3 PASS")
-        
+
 
 if __name__ == '__main__':
     unittest.main()

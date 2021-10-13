2aaca689de9958d7baaddb36762a418b7687190f
Vitaliy Baseckas <L00169827@student.lyit.ie>
2021-10-13 23:13:30 +0100

    say something

---
+++ pkg/Test_something.py
@@ -0,0 +1,10 @@
+# -------------------------------------------------------------------------------
+# Name:        Test_something.py
+# Project:     CA_Lab1
+#
+# Author:      Vitaliy Baseckas
+#
+# Created:     13.10.2021
+# Copyright:   (c) Vitaliy Baseckas 2021
+# Licence:     <your licence>
+# -------------------------------------------------------------------------------

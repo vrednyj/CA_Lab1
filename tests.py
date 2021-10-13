7a098194af06c8632a9ebda251f56c0d990ee19d
Vitaliy Baseckas <L00169827@student.lyit.ie>
2021-10-13 22:40:09 +0100

    No error

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from pkg import CA_Lab1,Maths
 
 class TestMethods(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(CA_Lab1.run(), 'This will print something to the scree')
+        self.assertEqual(CA_Lab1.run(), 'This will print something to the screen')
         self.run(Maths())
 
 

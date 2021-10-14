7cbc5899cbb4fa99d0ca4240a72b8da106c1e1fc
Vitaliy Baseckas <L00169827@student.lyit.ie>
2021-10-14 23:27:58 +0100

    Test condition has been changed

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ from pkg import CA_Lab1,Maths
 class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(CA_Lab1.run(), 'This will print something to the screen',"Test 1 PASS")
-        self.assertEqual(Maths.numbers(), "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]","Test 2 PASS")
+        self.assertEqual(Maths.numbers(), "[0, 1, 2, 3, 4, 5, 6, 7, 8, 1]","Test 2 PASS")
         self.assertNotEqual(Maths.numbers(), "[1, 2, 3, 4, 5, 6, 7, 8, 0]","Test 3 PASS")
 
 

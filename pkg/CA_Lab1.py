cfe851d433b2c4fc23e67dc2e05e6a4c9abf94c4
Vitaliy Baseckas <L00169827@student.lyit.ie>
2021-10-14 23:14:18 +0100

    Commit python-package.yml

--- pkg/CA_Lab1.py
+++ pkg/CA_Lab1.py
@@ -6,7 +6,7 @@
 #
 # Created:     13.10.2021
 # Copyright:   (c) Vitaliy Baseckas 2021
-# Licence:     <your licence>
+# Licence:     Free ware
 # -------------------------------------------------------------------------------
 def run():
     msg='This will print something to the screen'
